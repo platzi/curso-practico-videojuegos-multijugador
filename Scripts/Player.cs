612269edf1e73214fa1396dece8cedb0f10de366
Hector Pulido <hectorandrespp@gmail.com>
2021-10-26 15:25:33 -0500

    Creacion del personaje

---
+++ Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+
+    void FixedUpdate(){
+        rb.velocity = new Vector2(0,Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+}
