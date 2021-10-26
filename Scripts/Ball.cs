9749609678fd0b12776ca2cb1f5a5c80e4037ee1
Hector Pulido <hectorandrespp@gmail.com>
2021-10-26 16:25:38 -0500

    Creacion de la pelota y su movimiento

---
+++ Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Ball : NetworkBehaviour
+{
+    public float speed = 30;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.simulated = true;
+        rb.velocity = Vector2.left * speed;
+    }
+
+    float HitFactor(Vector2 ballPosition, Vector2 racketPosition, float racketHeight)
+    {
+        return (ballPosition.y - racketPosition.y) / racketHeight;
+    }
+
+    // Update is called once per frame
+    [ServerCallback]
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.GetComponent<Player>())
+        {
+            rb.velocity = Vector2.Reflect(rb.velocity, col.contacts[0].normal).normalized * speed;
+            float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+            float x = col.relativeVelocity.x > 0 ? 1 : -1;
+
+            Vector2 dir = new Vector2(x, y).normalized;
+            rb.velocity = dir * speed;
+        }
+    }
+}
--- Scripts/NetworkManagerPong.cs
+++ Scripts/NetworkManagerPong.cs
@@ -8,15 +8,28 @@ public class NetworkManagerPong : NetworkManager
     public Transform leftRacketSpawn;
     public Transform rightRacketSpawn;
 
+    public GameObject ballPrefab;
+    public GameObject ball;
+
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         Transform startPosition = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
         GameObject player = Instantiate(playerPrefab, startPosition.position, Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player);
+
+        if (numPlayers == 2){
+            ball = Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
+            NetworkServer.Spawn(ball);
+        }
     }
 
     public override void OnServerDisconnect(NetworkConnection conn)
     {
+        if (ball != null)
+        {
+            NetworkServer.Destroy(ball);
+        }
+
         base.OnServerDisconnect(conn);
     }
 }
