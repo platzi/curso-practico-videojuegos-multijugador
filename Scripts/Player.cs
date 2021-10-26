c83aeb85229185e3406c6bdfe3d9b7a6d0e1ab66
Hector Pulido <hectorandrespp@gmail.com>
2021-10-26 15:39:57 -0500

    Autoridad y movimiento del jugador

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     public float speed = 10f;
     public Rigidbody2D rb;
 
     void FixedUpdate(){
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         rb.velocity = new Vector2(0,Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
     }
 }
