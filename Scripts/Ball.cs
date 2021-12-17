250a5a8218773ad17a14bd73210c4715d2a6edfd
Hector Pulido <hectorandrespp@gmail.com>
2021-12-16 21:42:00 -0500

    Fin del juego

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -9,11 +9,13 @@ public class Ball : NetworkBehaviour
     public float speed = 30;
     public Rigidbody2D rb;
 
-    IEnumerator StartBall()
+    IEnumerator StartBall(float delay)
     {
         rb.simulated = false;
         rb.velocity = Vector2.zero;
         transform.position = Vector2.zero;
+        yield return new WaitForSeconds(delay);
+        RpcDisableWinState();
         yield return new WaitForSeconds(2);
         rb.simulated = true;
         float direction = Random.Range(0f, 1f) > 0.5f ? 1 : -1;
@@ -23,13 +25,16 @@ public class Ball : NetworkBehaviour
     // Start is called before the first frame update
     public override void OnStartServer()
     {
-        StartCoroutine(StartBall());
+        StartCoroutine(StartBall(0));
         networkManager = FindObjectOfType<NetworkManagerPong>();
     }
 
     void Start()
     {
-        textScore = FindObjectOfType<TMP_Text>();
+        textScore = GameObject.Find("ScoreText").GetComponent<TMP_Text>();
+        winnerScore = GameObject.Find("WinnerPlayerText").GetComponent<TMP_Text>();
+
+        winnerScore.gameObject.SetActive(false);
     }
 
     float HitFactor(Vector2 ballPosition, Vector2 racketPosition, float racketHeight)
@@ -56,24 +61,55 @@ public class Ball : NetworkBehaviour
     public int leftScore;
     public int rightScore;
     public TMP_Text textScore;
+    public TMP_Text winnerScore;
 
     [ServerCallback]
     void Update()
     {
+        int delay = 0;
         if (transform.position.x > networkManager.rightRacketSpawn.position.x)
         {
             leftScore++;
-            StartCoroutine(StartBall());
+            print(leftScore);
+            if (leftScore >= 2)
+            {
+                RpcWinState("Izquierdo");
+                delay = 5;
+                leftScore = 0;
+                rightScore = 0;
+            }
+            StartCoroutine(StartBall(delay));
             RpcUpdateTextScore(leftScore, rightScore);
         }
+
         if (transform.position.x < networkManager.leftRacketSpawn.position.x)
         {
             rightScore++;
-            StartCoroutine(StartBall());
+            print(rightScore);
+            if (rightScore >= 2)
+            {
+                RpcWinState("Derecho");
+                delay = 5;
+                leftScore = 0;
+                rightScore = 0;
+            }
+            StartCoroutine(StartBall(delay));
             RpcUpdateTextScore(leftScore, rightScore);
         }
     }
 
+    [ClientRpc]
+    public void RpcWinState(string winner)
+    {
+        winnerScore.gameObject.SetActive(true);
+        winnerScore.text = $"Ha ganado el jugador del lado <b>{winner}</b>";
+    }
+
+    [ClientRpc]
+    public void RpcDisableWinState()
+    {
+        winnerScore.gameObject.SetActive(false);
+    }
 
     [ClientRpc]
     public void RpcUpdateTextScore(int leftScore, int rightScore)
